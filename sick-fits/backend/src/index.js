0c1189b6c0210fde518dd0ff1ce43c2dab6a87c1
gemcfadyen <gemcfadyen@hotmail.co.uk>
2019-04-14 09:21:36 +0100

    Added the ability to create users to the backend

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,3 +1,4 @@
+const cookieParser = require("cookie-parser");
 //start up node server, entry point of the application
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
@@ -7,6 +8,7 @@ const db = require("./db");
 const server = createServer();
 
 //use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
 //use express middleware to populate current user
 
 server.start(
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,4 +1,6 @@
 // Where we interact with the Prisma database
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -15,27 +17,47 @@ const Mutations = {
   },
   updateItem(parent, args, ctx, info) {
     //copy of updates
-    const updates = { ...args };
+    const updates = {...args};
     delete updates.id;
     return ctx.db.mutation.updateItem(
       {
         data: updates,
-        where: { id: args.id }
+        where: {id: args.id}
       },
       info
     );
   },
 
   async deleteItem(parent, args, ctx, info) {
-    const where = { id: args.id };
+    const where = {id: args.id};
     //find the item
-    const item = await ctx.db.query.item({ where }, "{id, title}");
+    const item = await ctx.db.query.item({where}, "{id, title}");
 
     //check the user has the permissions to delete
     //todo
 
     //delete it
     return ctx.db.mutation.deleteItem({where}, info)
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    args.password = await bcrypt.hash(args.password, 10);
+
+    const user = ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        permissions: {set: ["USER"]}
+      }
+    }, info);
+
+    //create jwt token for the user
+    const token = jwt.sign({userId: user.id}, process.env.APP_SECRET)
+    //set jwt as cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 //one year cookie
+    })
+    return user;
   }
 };
 
