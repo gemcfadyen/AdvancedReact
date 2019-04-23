795413b6ec11ecf8334d0a27208fa9bebd32e0d4
gemcfadyen <gemcfadyen@hotmail.co.uk>
2019-04-23 16:45:38 +0100

    Add a relationship between the user and the item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -7,9 +7,18 @@ const { makeANiceEmail, transport } = require("../mail");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
+    if(!ctx.request.userId) {
+      throw new Error("You must be logged in to do that");
+    }
     const item = await ctx.db.mutation.createItem(
       {
         data: {
+          //this is how we create a relationship between the item and the user
+          user: {
+            connect: {
+              id: ctx.request.userId
+            }
+          },
           ...args //because all the item details are in the args, we can spread them here
         }
       },
